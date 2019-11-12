230324f4676c526138238046b34aea2377a6bb61
Backes, Tobias <tobias.backes@gesis.org>
2019-11-12 19:01:56 +0100

    added a few improvements in the parsing process and a large database to look up cities in

--- csv2json.py
+++ csv2json.py
@@ -2,10 +2,14 @@ import sys
 import csv
 import json
 import re
+import sqlite3
 
 infile  = sys.argv[1];
 outfile = sys.argv[2];
 
+_geo_  = 'resources/allCountries.db';
+_con_  = sqlite3.connect(_geo_);
+_cur_  = _con_.cursor();
 _empty = set(['','n','X']);
 
 def clean_rows(rows,index2column):
@@ -15,16 +19,34 @@ def clean_rows(rows,index2column):
                 rows[i][j] = None;
             rows[i][j] = clean(rows[i][j],index2column[j]);
 
+def is_city(string): #TODO: Could even disambiguate with other information extracted #TODO: Should also normalize string with ascii column from DB
+    freq = _cur_.execute("SELECT COUNT(DISTINCT geonameid) FROM alternatives WHERE alternative=?",(string.decode('utf-8'),)).fetchall()[0][0];
+    if freq > 0:
+        types = set([row[0] for row in _cur_.execute("SELECT feature_class FROM geonames WHERE geonameid IN (SELECT DISTINCT geonameid FROM alternatives WHERE alternative=?)",(string.decode('utf-8'),)).fetchall()]);
+        if ('P' in types or 'S' in types):
+            print string, 'is a city.';
+            return True;
+    print string, 'is not a city.';
+    return False;
+
 def clean(value,field):
     if value == None:
         return None;
     if field == 'span':
-        return tuple(re.findall(r'\d\d\d\d',value));
+        dates = re.findall(r'\d\d\d\d',value);
+        return {'from':dates[0], 'to':dates[-1]} if len(dates)==2 else {'from':dates[0], 'to':None} if len(dates)==1 else {'from':None, 'to':None} if len(dates)==0 else {'from':dates[0], 'to':dates[-1], 'other':dates[1:-2]};
     if field == 'sofis_id':
         try:
             return int(value);
         except:
             return value;
+    if field == 'institution_l1':
+        match = re.search(r'\(.+\)',value);
+        if match and is_city(match.group()[1:-1]):
+            span = match.span();
+            return {'name':value[:span[0]].strip(), 'city':value[span[0]+1:span[1]-1]};
+        else:
+            return {'name':value, 'city':None};
     return value;
 
 IN           = open(infile,'r');
